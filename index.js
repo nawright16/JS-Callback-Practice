939e503579f835e9465c9b0ea52e408fa1fa4eff
Nick Wright <nwright@egnyte.com>
2022-12-20 10:45:50 -0500

    fixed most code, character moves, some items missing.

--- index.js
+++ index.js
@@ -1,30 +1,78 @@
-//const inventory = newInventory()
-//move(inventory).to(0, 0)
+const inventory = newInventory()
+//move(inventory).withArrowKeys(0, 0)
 
-const character = newImage('assets/green-character/static.gif').
+const character = newImage('assets/green-character/static.gif')
+//const character = newImage('assets/green-character/static.gif')
 
-function handleDirectionChange() {
-if(direction === null){
-    character.src = 'assets/green-character/static.gif'
-}
-if(direction === 'west'){
-    character.src = 'assets/green-character/west.gif'
-}
-if(direction === 'north'){
-    character.src = 'assets/green-character/north.gif'
-}
-if(direction === 'east'){
-    character.src = 'assets/green-character/east.gif'
-}
-if(direction === 'south'){
-    character.src = 'assets/green-character/south.gif'
-}
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
+    if(direction === 'west'){
+        character.src = 'assets/green-character/west.gif'
+    }
+    if(direction === 'north'){
+        character.src = 'assets/green-character/north.gif'
+    }
+    if(direction === 'east'){
+        character.src = 'assets/green-character/east.gif'
+    }
+    if(direction === 'south'){
+        character.src = 'assets/green-character/south.gif'
+    }
 }
 
 move(character).withArrowKeys(100, 250, handleDirectionChange)
 
+//move(character).withArrowKeys(100, 250)
 
-move(newImage('assets/tree.png')).withArrowKeys(200, 450)
+
+// function moveCharacter() {
+//     if (direction === 'west') {
+//         x = x - 1
+//     }
+
+//     if (direction === 'north') {
+//         y = y + 1
+//     }
+
+//     if (direction === 'east') {
+//         x = x + 1
+//     }
+
+//     if (direction === 'south') {
+//         y = y - 1
+//     }
+
+//     character.style.left = x + 'px'
+//     character.style.bottom = y + 'px'
+//     }
+
+//setInterval (moveCharacter, 1)
+
+
+// document.addEventListener('keydown', function(e){                
+//     if(e.key === 'ArrowLeft'){
+//         direction = 'west'
+//     }
+//     if(e.key === 'ArrowUp'){
+//         direction = 'north'
+//     }
+//     if(e.key === 'ArrowRight'){
+//         direction = 'east'
+//     }
+//     if(e.key === 'ArrowDown'){
+//         direction = 'south'
+//     }
+// })
+
+// document.addEventListener('keyup', function(e){
+//     direction = null
+// })
+
+
+
+move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
 move(newImage('assets/crate.png')).to(150, 350)
